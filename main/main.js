350d22bc5235085ad1b549495deacdaa7e6408bd
ldliang93 <ldliang93@163.com>
2018-04-27 11:52:08 +0800

    4A0B 0A4B

--- main/main.js
+++ main/main.js
@@ -1,6 +1,16 @@
 // Write your cade below:
-function main() {};
 
-module.exports = {
-    main
-}
+
+module.exports = function  guessNumber(input,output) {
+var countA = 0;
+var countB = 0;
+  if(input === output){
+    return '4A0B';
+  }
+  if(output.indexOf('1')>-1 && output.indexOf('2')>-1 && output.indexOf('3')>-1 && output.indexOf('4')>-1 ){
+    countA = 0;
+    countB = 4;
+   }
+   return countA + 'A' + countB +'B'
+
+};
--- spec/main-spec.js
+++ spec/main-spec.js
@@ -1,5 +1,22 @@
-const main = require('../main/main');
+const guessNumber = require('../main/main');
 
 describe('main()', () => {
+
+  it('should return 4A0B when guess 1234', function() {
+    let setNum = '1234';
+    let guessNum = '1234';
+    let result = guessNumber(setNum,guessNum);
+    let expected = '4A0B';
+    expect(result).toEqual(expected)
+  });
+
+  it('should return 0A4B when guess 4321', function() {
+    let setNum = '1234';
+    let guessNum = '4321';
+    let result = guessNumber(setNum,guessNum);
+    let expected = '0A4B';
+    expect(result).toEqual(expected)
+  });
+
 
 });
